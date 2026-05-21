7e86e44826522918220a494798994cb10d164782
Indra Maulana <indramaulana4455@gmail.com>
2026-05-21 21:23:24 +0700

    ready

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,14 @@
-async function name(params:type) {
-  import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-  import { User, Kategori, Berita } from '../entities';
-  
-  export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'sqlite',
-    database: process.env.DB_NAME || 'prokopim.db',
-    entities: [User, Kategori, Berita],
-    synchronize: process.env.NODE_ENV !== 'production',
-    logging: process.env.NODE_ENV !== 'production',
-  };
-  
-}
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User, Kategori, Berita } from '../entities';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: (process.env.DB_TYPE as any) || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'admin123',
+  database: process.env.DB_NAME || 'postgres',
+  entities: [User, Kategori, Berita],
+  synchronize: process.env.NODE_ENV !== 'production',
+  logging: process.env.NODE_ENV !== 'production',
+};
